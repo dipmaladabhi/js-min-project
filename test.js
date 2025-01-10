2b07076d5f64b2eca11ab2e13905dabb1d6cf4cf
dipmaladabhi <145713834+dipmaladabhi@users.noreply.github.com>
2025-01-10 23:27:17 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+/* prefer not to use var
+because of issue in block scope and functional scope */
+
+// /* "use strict"; treat all code as new JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browsing;
+
+// data type 
+// null : console.log(typeof null); ans : Object
+// undefined : console.log(typeof undefined); ans : undefined
+
+console.log ("Dipmala");
